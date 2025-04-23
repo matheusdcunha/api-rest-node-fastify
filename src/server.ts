d0b17db12fce00b05f58ad4049b2f814cc470539
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-04-23 13:54:07 -0300

    feat: Add cookies for auth

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import fastify from 'fastify'
 import { env } from './env'
+import cookie from '@fastify/cookie'
 import { transactionsRoutes } from './routes/transactions'
 
 export const app = fastify()
 const port = env.PORT
+
+app.addHook('preHandler', async () => {
+  console.log('oi')
+})
+app.register(cookie)
 
 app.register(transactionsRoutes, {
   prefix: '/transactions',
