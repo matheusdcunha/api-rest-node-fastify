d94f0f83140daef7e587ac4d2f7db0f4bcf7e39f
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-04-22 22:59:17 -0300

    feat: Transactions Routes

---
+++ src/routes/transactions.ts
@@ -0,0 +1,52 @@
+import type { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '../database'
+import { randomUUID } from 'node:crypto'
+
+export function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'ammount' })
+      .first()
+
+    return { summary }
+  })
+
+  app.post('/', async (request, reply) => {
+    const createTransactionsBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionsBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
-const app = fastify()
+export const app = fastify()
 const port = env.PORT
 
-app.get('/hello', async () => {
-  const teste = await knex('sqlite_schema').select('*')
-  console.log(teste)
-  return teste
+app.register(transactionsRoutes, {
+  prefix: '/transactions',
 })
 
 app
